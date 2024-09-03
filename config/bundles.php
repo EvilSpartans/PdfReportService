ab95edad16af93080fd74936d9770e4425d6a336
Alexis Moren <alexismoren62@hotmail.com>
2024-09-03 17:09:39 +0200

    ready prod

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
